2cb06abb6292889c68708813fd37360923afdf8c
olevep <olevep@gmail.com>
2025-02-13 21:23:59 +0300

    edit hw14.md

---
+++ 14/create_bucket.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Example: ./create_bucket.sh example.url.com username password bucket-name
+
+if [ -z $1 ]; then
+  echo "You have NOT specified a MINIO URL!"
+  exit 1
+fi
+
+if [ -z $2 ]; then
+  echo "You have NOT specified a USERNAME!"
+  exit 1
+fi
+
+if [ -z $3 ]; then
+  echo "You have NOT specified a PASSWORD!"
+  exit 1
+fi
+
+if [ -z $4 ]; then
+  echo "You have NOT specified a BUCKET!"
+  exit 1
+fi
+
+# User Minio Vars
+URL=$1
+USERNAME=$2
+PASSWORD=$3
+BUCKET=$4
+MINIO_PATH="/${BUCKET}"
+
+# Static Vars
+DATE=$(date -R --utc)
+CONTENT_TYPE='any'
+SIG_STRING="PUT\n\n${CONTENT_TYPE}\n${DATE}\n${MINIO_PATH}"
+SIGNATURE=`echo -en ${SIG_STRING} | openssl sha1 -hmac ${PASSWORD} -binary | base64`
+
+
+curl -v -X PUT \
+    -H "Host: $URL" \
+    -H "Date: ${DATE}" \
+    -H "Content-Type: ${CONTENT_TYPE}" \
+    -H "Authorization: AWS ${USERNAME}:${SIGNATURE}" \
+    http://$URL${MINIO_PATH}
---
+++ 14/upload_to_minio.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Example: ./upload_to_minio.sh example.url.com username password bucket-name minio/path/to/file.txt /upload/path/from/file.txt
+
+if [ -z $1 ]; then
+  echo "You have NOT specified a MINIO URL!"
+  exit 1
+fi
+
+if [ -z $2 ]; then
+  echo "You have NOT specified a USERNAME!"
+  exit 1
+fi
+
+if [ -z $3 ]; then
+  echo "You have NOT specified a PASSWORD!"
+  exit 1
+fi
+
+if [ -z $4 ]; then
+  echo "You have NOT specified a BUCKET!"
+  exit 1
+fi
+
+if [ -z $5 ]; then
+  echo "You have NOT specified a MINIO FILE PATH!"
+  exit 1
+fi
+
+if [ -z $6 ]; then
+  echo "You have NOT specified a UPLOAD FILE PATH!"
+  exit 1
+fi
+
+
+# User Minio Vars
+URL=$1
+USERNAME=$2
+PASSWORD=$3
+BUCKET=$4
+MINIO_PATH="/${BUCKET}/$5"
+UPLOAD_FILE=$6
+
+# Static Vars
+DATE=$(date -R --utc)
+CONTENT_TYPE='application/zstd'
+SIG_STRING="PUT\n\n${CONTENT_TYPE}\n${DATE}\n${MINIO_PATH}"
+SIGNATURE=`echo -en ${SIG_STRING} | openssl sha1 -hmac ${PASSWORD} -binary | base64`
+
+
+curl -v -X PUT -T "${UPLOAD_FILE}" \
+    -H "Host: $URL" \
+    -H "Date: ${DATE}" \
+    -H "Content-Type: ${CONTENT_TYPE}" \
+    -H "Authorization: AWS ${USERNAME}:${SIGNATURE}" \
+    http://$URL${MINIO_PATH}
